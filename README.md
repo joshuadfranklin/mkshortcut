188089e2222e3a5c53009fc2f552e9d01240660b
Joshua Daniel Franklin <joshuadfranklin@yahoo.com>
2014-12-14 07:33:12 -0600

    mkshortcut stuff

--- README.md
+++ README.md
@@ -4,15 +4,21 @@ I now rarely use Windows but still think Cygwin and derivatives like MobaXterm m
 
 
 * Wrote mkshortcut, a GNU-style Cygwin command line utility to create a Shortcut (OLE Shell Link)
-* * Included in cygutils with small changes over the years
-* * Handy options such as "All Users" and "Start Menu/Programs"
-* * I was not great at debugging https://cygwin.com/ml/cygwin/2002-03/msg00055.html
+  * Included in cygutils with small changes over the years
+  * Handy options such as "All Users" and "Start Menu/Programs"
+  * I was not great at debugging https://cygwin.com/ml/cygwin/2002-03/msg00055.html
+  * Example: Create a shortuct to rxvt on the Desktop that has Internet Explorer's icon but really starts up bash:
+```
+mkshortcut -a '-rv -fn "FixedSys" -e /bin/bash --login -i' \
+-i /c/WINNT/system32/SHELL32.DLL -j 106 -n "Internet Explorer" \
+-D /bin/rxvt
+```
 
 * Maintained and added to the Cygwin documenation 
-* * User Guide, FAQs, cygwin-doc package building and distribution
-* * Gold Stars https://cygwin.com/goldstars/
+  * User Guide, FAQs, cygwin-doc package building and distribution
+  * Gold Stars https://cygwin.com/goldstars/
 
 * Created Cygwin-lite Before Cygwin's setup.exe installer did a minimal install
-* * http://cygwin-lite.sourceforge.net/
-* * Beginner's Guide tutorial http://cygwin-lite.sourceforge.net/html/begin.html
-* * Fit on a 3.5" floppy, and on my tiny computer lab network space
+  * http://cygwin-lite.sourceforge.net/
+  * Beginner's Guide tutorial http://cygwin-lite.sourceforge.net/html/begin.html
+  * Fit on a 3.5" floppy, and on my tiny computer lab network space
--- mkshortcut.c
+++ mkshortcut.c
@@ -1,6 +1,5 @@
 /* mkshortcut.c -- create a Windows shortcut 
  *
- * Version 1.01
  * Copyright (c) 2002 Joshua Daniel Franklin
  *
  * This program is free software; you can redistribute it and/or
@@ -25,6 +24,7 @@
  *   3: windows error (interface failed)
  * 
  * Compile with: gcc -o prog.exe mkshortcut.c -lole32 -luuid
+ *  (You'd need to uncomment the moved to common.h lines.)
  *
  */
 #if HAVE_CONFIG_H
@@ -42,21 +42,20 @@
 #include <getopt.h>
 */
 
-static const char versionID[] =
-    "1.01.0";
+static const char version[] = "$Revision: 1.2 $";
+static const char versionID[] = "1.01.0";
 /* for CVS */
 static const char revID[] =
   "$Id: mkshortcut.c,v 1.2 2002/02/24 03:41:44 cwilson Exp $";
 static const char copyrightID[] =
-    "Copyright (c) 2001,...\nJoshua Daniel Franklin. All rights reserved.\nLicensed under GPL v2.0\n";
+  "Copyright (c) 2002\nJoshua Daniel Franklin. All rights reserved.\nLicensed under GPL v2.0\n";
 
 static char *prog_name;
 static char *argument_arg, *name_arg;
 static int icon_flag, unix_flag, windows_flag;
 static int allusers_flag, desktop_flag, smprograms_flag;
 
-static struct option long_options[] =
-{
+static struct option long_options[] = {
   {"arguments", required_argument, NULL, 'a'},
   {"help", no_argument, NULL, 'h'},
   {"icon", required_argument, NULL, 'i'},
@@ -88,18 +87,56 @@ NOTE: All filename arguments must be in unix (POSIX) format\n\
   exit (status);
 }
 
+print_version ()
+{
+  const char *v = strchr (version, ':');
+  int len;
+  if (!v)
+    {
+      v = "?";
+      len = 1;
+    }
+  else
+    {
+      v += 2;
+      len = strchr (v, ' ') - v;
+    }
+  printf ("\
+mkshortcut (cygutils) %.*s\n%s\
+", len, v, copyrightID);
+}
+
 int
 main (int argc, char **argv)
 {
+  int opt, tmp, offset;
+  char *args, *tmp_str, *buf_str;
+  char link_name[MAX_PATH];
+  char exe_name[MAX_PATH];
+  char dir_name[MAX_PATH];
+  char icon_name[MAX_PATH];
+
+  /* For OLE interface */
   LPITEMIDLIST id;
   HRESULT hres;
-  IShellLink *sl;
-  IPersistFile *pf;
+  IShellLink *shell_link;
+  IPersistFile *persist_file;
   WCHAR widepath[MAX_PATH];
 
-  int opt, tmp, offset;
-  char *args, *str2, *str1;
-  char lname[MAX_PATH], exename[MAX_PATH], dirname[MAX_PATH], iconname[MAX_PATH];
+  buf_str = (char *) malloc (PATH_MAX);
+  if (buf_str == NULL)
+    {
+      fprintf (stderr, "%s: out of memory\n", prog_name);
+      exit (2);
+    }
+
+  offset = 0;
+  icon_flag = 0;
+  unix_flag = 0;
+  windows_flag = 0;
+  allusers_flag = 0;
+  desktop_flag = 0;
+  smprograms_flag = 0;
 
   prog_name = strrchr (argv[0], '/');
   if (prog_name == NULL)
@@ -109,15 +146,9 @@ main (int argc, char **argv)
   else
     prog_name++;
 
-  offset=0;
-  icon_flag=0;
-  unix_flag=0;
-  windows_flag=0;
-  allusers_flag=0;
-  desktop_flag=0;
-  smprograms_flag=0;
-
-  while ( (opt = getopt_long(argc, argv, (char *) "a:i:j:n:hvADP", long_options, NULL) ) != EOF)
+  while ((opt =
+	  getopt_long (argc, argv, (char *) "a:i:j:n:hvADP", long_options,
+		       NULL)) != EOF)
     {
       switch (opt)
 	{
@@ -129,7 +160,7 @@ main (int argc, char **argv)
 	  break;
 	case 'i':
 	  icon_flag = 1;
-	cygwin_conv_to_full_win32_path(optarg, iconname);
+	  cygwin_conv_to_full_win32_path (optarg, icon_name);
 	  break;
 	case 'j':
 	  if (!icon_flag)
@@ -140,8 +171,7 @@ main (int argc, char **argv)
 	  name_arg = optarg;
 	  break;
 	case 'v':
-	printf ("Cygwin shortcut creator version 1.01\n");
-	printf ("Copyright 2002 Joshua Daniel Franklin.\n");
+	  print_version ();
 	  exit (0);
 	case 'A':
 	  allusers_flag = 1;
@@ -165,105 +195,112 @@ main (int argc, char **argv)
   if (optind != argc - 1)
     usage (stderr, 1);
 
-  // If there's a colon in the TARGET, it should be a URL
+  /*  If there's a colon in the TARGET, it should be a URL */
   if (strchr (argv[optind], ':') != NULL)
     {
-    // Nope, somebody's trying a W32 path 
+      /*  Nope, somebody's trying a W32 path  */
       if (argv[optind][1] == ':')
 	usage (stderr, 1);
-    strcpy(exename, argv[optind]);
-    dirname[0]='\0';  //No working dir for URL
+      strcpy (exe_name, argv[optind]);
+      dir_name[0] = '\0';	/* No working dir for URL */
     }
+  /* Convert TARGET to win32 path */
   else
     {
-    strcpy(str1, argv[optind]);
-    cygwin_conv_to_full_win32_path(str1, exename);
+      strcpy (buf_str, argv[optind]);
+      cygwin_conv_to_full_win32_path (buf_str, exe_name);
 
-    // Get a working dir from the exepath
-    str1 = strrchr(exename, '\\');
-    tmp = strlen (exename) - strlen (str1);
-    strncpy (dirname, exename, tmp);
-    dirname[tmp]='\0';
+      /*  Get a working dir from the exepath */
+      tmp_str = strrchr (exe_name, '\\');
+      tmp = strlen (exe_name) - strlen (tmp_str);
+      strncpy (dir_name, exe_name, tmp);
+      dir_name[tmp] = '\0';
     }
 
-  // Generate a name for the link if not given
+  /*  Generate a name for the link if not given */
   if (name_arg == NULL)
     {
-    // Strip trailing /'s if any
-    str2 = str1 = argv[optind];
-    tmp = strlen(str1) - 1;
-    while ( strrchr(str1,'/') == (str1+tmp) )
+      /*  Strip trailing /'s if any */
+      strcpy (buf_str, argv[optind]);
+      tmp_str = buf_str;
+      tmp = strlen (buf_str) - 1;
+      while (strrchr (buf_str, '/') == (buf_str + tmp))
 	{
-      str1[tmp]='\0';
+	  buf_str[tmp] = '\0';
 	  tmp--;
 	}
-    // Get basename
-    while (*str1)
+      /*  Get basename */
+      while (*buf_str)
 	{
-      if (*str1 == '/')
-        str2 = str1 + 1;
-      str1++;
+	  if (*buf_str == '/')
+	    tmp_str = buf_str + 1;
+	  buf_str++;
 	}
+      strcpy (link_name, tmp_str);
     }
+  /*  User specified a name, so check it and convert  */
   else
     {
-    // User specified a name, so check it and convert 
       if (desktop_flag || smprograms_flag)
 	{
-      // Absolute path not allowed on Desktop/SM Programs
+	  /*  Cannot have absolute path relative to Desktop/SM Programs */
 	  if (name_arg[0] == '/')
 	    usage (stderr, 1);
 	}
-    // Sigh. Another W32 path
+      /*  Sigh. Another W32 path */
       if (strchr (name_arg, ':') != NULL)
 	usage (stderr, 1);
-    cygwin_conv_to_win32_path(name_arg, str2);
+      cygwin_conv_to_win32_path (name_arg, link_name);
     }
 
-  // Add suffix to link name if necessary
-  if (strlen(str2) > 4)
+  /*  Add suffix to link name if necessary */
+  if (strlen (link_name) > 4)
     {
-    tmp = strlen(str2) - 4;
-    if (strncmp (str2+tmp, ".lnk", 4) != 0 )
-      strcat (str2, ".lnk");
+      tmp = strlen (link_name) - 4;
+      if (strncmp (link_name + tmp, ".lnk", 4) != 0)
+	strcat (link_name, ".lnk");
     }
-  else strcat (str2, ".lnk");
-  strcpy (lname, str2);
+  else
+    strcat (link_name, ".lnk");
 
-  // Prepend relative path if necessary 
+  /*  Prepend relative path if necessary  */
   if (desktop_flag)
     {
+      strcpy (buf_str, link_name);
       if (!allusers_flag)
 	SHGetSpecialFolderLocation (NULL, CSIDL_DESKTOPDIRECTORY, &id);
       else
 	SHGetSpecialFolderLocation (NULL, CSIDL_COMMON_DESKTOPDIRECTORY, &id);
-    SHGetPathFromIDList(id, lname);
-    // Make sure Win95 without "All Users" has output 
-    if ( strlen(lname) == 0 ) {
+      SHGetPathFromIDList (id, link_name);
+      /*  Make sure Win95 without "All Users" has output  */
+      if (strlen (link_name) == 0)
+	{
 	  SHGetSpecialFolderLocation (NULL, CSIDL_DESKTOPDIRECTORY, &id);
-      SHGetPathFromIDList(id, lname);
+	  SHGetPathFromIDList (id, link_name);
 	}
-    strcat(lname, "\\");
-    strcat(lname, str2);
+      strcat (link_name, "\\");
+      strcat (link_name, buf_str);
     }
 
   if (smprograms_flag)
     {
+      strcpy (buf_str, link_name);
       if (!allusers_flag)
 	SHGetSpecialFolderLocation (NULL, CSIDL_PROGRAMS, &id);
       else
 	SHGetSpecialFolderLocation (NULL, CSIDL_COMMON_PROGRAMS, &id);
-    SHGetPathFromIDList(id, lname);
-    // Make sure Win95 without "All Users" has output 
-    if ( strlen(lname) == 0 ) {
+      SHGetPathFromIDList (id, link_name);
+      /*  Make sure Win95 without "All Users" has output  */
+      if (strlen (link_name) == 0)
+	{
 	  SHGetSpecialFolderLocation (NULL, CSIDL_PROGRAMS, &id);
-      SHGetPathFromIDList(id, lname);
+	  SHGetPathFromIDList (id, link_name);
 	}
-    strcat(lname, "\\");
-    strcat(lname, str2);
+      strcat (link_name, "\\");
+      strcat (link_name, buf_str);
     }
 
-  // Beginning of Windows interface
+  /*  Beginning of Windows interface */
   hres = OleInitialize (NULL);
   if (hres != S_FALSE && hres != S_OK)
     {
@@ -271,29 +308,47 @@ main (int argc, char **argv)
       exit (3);
     }
 
-  hres = CoCreateInstance(&CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, &IID_IShellLink, (void **) &sl);
+  hres =
+    CoCreateInstance (&CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER,
+		      &IID_IShellLink, (void **) &shell_link);
   if (SUCCEEDED (hres))
     {
-    hres = sl->lpVtbl->QueryInterface(sl, &IID_IPersistFile, (void **) &pf);
+      hres =
+	shell_link->lpVtbl->QueryInterface (shell_link, &IID_IPersistFile,
+					    (void **) &persist_file);
       if (SUCCEEDED (hres))
 	{
-      sl->lpVtbl->SetPath (sl, exename);
-      cygwin_conv_to_full_posix_path(exename, str1);
-      sl->lpVtbl->SetDescription (sl, str1);
-      sl->lpVtbl->SetWorkingDirectory (sl, dirname);
-      if (argument_arg) sl->lpVtbl->SetArguments (sl, argument_arg);
-      if (icon_flag) sl->lpVtbl->SetIconLocation (sl, iconname, offset);
+	  shell_link->lpVtbl->SetPath (shell_link, exe_name);
+	  /* Put the POSIX path in the "Description", just to be nice */
+	  cygwin_conv_to_full_posix_path (exe_name, buf_str);
+	  shell_link->lpVtbl->SetDescription (shell_link, buf_str);
+	  shell_link->lpVtbl->SetWorkingDirectory (shell_link, dir_name);
+	  if (argument_arg)
+	    shell_link->lpVtbl->SetArguments (shell_link, argument_arg);
+	  if (icon_flag)
+	    shell_link->lpVtbl->SetIconLocation (shell_link, icon_name,
+						 offset);
 
-      // Make link name Unicode-compliant 
-      MultiByteToWideChar (CP_ACP, 0, lname, -1, widepath, MAX_PATH);
-      hres = pf->lpVtbl->Save (pf, widepath, TRUE);
+	  /*  Make link name Unicode-compliant  */
+	  hres =
+	    MultiByteToWideChar (CP_ACP, 0, link_name, -1, widepath,
+				 MAX_PATH);
 	  if (!SUCCEEDED (hres))
 	    {
-        fprintf(stderr, "%s: Save to persistant storage failed (Does the directory you are writing to exist?)\n", prog_name);
+	      fprintf (stderr, "%s: Unicode translation failed%d\n",
+		       prog_name, hres);
 	      exit (3);
 	    }
-      pf->lpVtbl->Release (pf);
-      sl->lpVtbl->Release (sl);
+	  hres = persist_file->lpVtbl->Save (persist_file, widepath, TRUE);
+	  if (!SUCCEEDED (hres))
+	    {
+	      fprintf (stderr,
+		       "%s: Saving \"%s\" failed; does the target directory exist?\n",
+		       prog_name, link_name);
+	      exit (3);
+	    }
+	  persist_file->lpVtbl->Release (persist_file);
+	  shell_link->lpVtbl->Release (shell_link);
 	}
       else
 	{
@@ -306,4 +361,6 @@ main (int argc, char **argv)
       fprintf (stderr, "%s: CoCreateInstance failed\n", prog_name);
       exit (3);
     }
+
+  exit (0);
 }
